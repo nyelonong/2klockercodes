1fddaaef81954b7b3c870959262d5377c4fbef4b
Muhammad Zaki Al-Afrani <zaki.afrani@gmail.com>
2020-08-21 01:53:47 +0700

    handle empty code

--- 2k.py
+++ 2k.py
@@ -37,7 +37,9 @@ for edge in edges:
         
     msg += f"Title: {title}\nCode: {code}\nCreated At: {create.astimezone(tz=jakarta)}\nExpire At: {expire.astimezone(tz=jakarta)}\n\n"
 
+if msg == '':
+    msg = 'No code today :('
+
 params = {'chat_id': tg_chat_id, 'text': msg}
 res = requests.post(f"https://api.telegram.org/bot{tg_token}/sendMessage", data=params).json()
-
-print(res)
+print(res["ok"])    
